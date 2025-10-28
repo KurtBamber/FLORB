ac3cbd2bbdf6238a91844dac53b398a4aa9c3b31
Kurtjbamber <Kurtbamber@icloud.com>
2025-10-28 18:04:24 +0000

    Added Grid
    Added a simple grid that tracks mouse position, can be expanded to allow tile placing.

---
+++ Assets/Scripts/Managers/GridManager.cs
@@ -0,0 +1,56 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridManager : MonoBehaviour
+{
+    [SerializeField] private float tileSize = 1f;
+    private Dictionary<Vector2Int, GameObject> placedTiles = new();
+    private Vector2Int hoveredTile;
+
+    private void Update()
+    {
+        Vector3 mouseToWorld = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mouseToWorld.z = 0f;
+        hoveredTile = WorldToGrid(mouseToWorld);
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("Clicked " + hoveredTile);
+        }
+    }
+
+    public Vector2Int WorldToGrid(Vector3 worldPos)
+    {
+        int x = Mathf.RoundToInt(worldPos.x / tileSize);
+        int y = Mathf.RoundToInt(worldPos.y / tileSize);
+        return new Vector2Int(x, y);
+    }
+
+    public Vector3 GridToWorld(Vector2Int gridPos)
+    {
+        return new Vector3(gridPos.x * tileSize, gridPos.y * tileSize, 0);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.gray;
+
+        for (int x = 0; x < 20; x++)
+        {
+            for (int y = 0; y < 50; y++)
+            {
+                Vector3 pos = new Vector3(x * tileSize, y * tileSize, 0);
+                Gizmos.DrawWireCube(pos, new Vector3(1, 1, 0) * tileSize);
+            }
+        }
+
+        Gizmos.color = Color.yellow;
+        Vector3 hoveredToWorld = GridToWorld(hoveredTile);
+        Gizmos.DrawWireCube(hoveredToWorld, new Vector3(1, 1, 0) * tileSize);
+    }
+
+    public bool IsTileOccupied(Vector2Int gridPos)
+    {
+        return placedTiles.ContainsKey(gridPos);
+    }
+}
