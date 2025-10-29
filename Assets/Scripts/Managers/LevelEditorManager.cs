b29a19608b8a1f7e8c6c290c06cb3efd51139cbf
Kurtjbamber <Kurtbamber@icloud.com>
2025-10-29 16:05:44 +0000

    Category Bar
    Added a ui element within the level editor that lets you select which type of object you want to place.

--- Assets/Scripts/Managers/LevelEditorManager.cs
+++ Assets/Scripts/Managers/LevelEditorManager.cs
@@ -1,4 +1,7 @@
+using NUnit.Framework;
 using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
 
 [System.Serializable]public class TileCategory
 {
@@ -9,8 +12,17 @@ using UnityEngine;
 public class LevelEditorManager : MonoBehaviour
 {
     [SerializeField] private TileCategory[] tileCategories;
+    [SerializeField] private Transform categoryBar;
+    [SerializeField] private GameObject categoryButtonPrefab;
     private int selectedCategoryIndex = 0;
     private int selectedVariantIndex = 0;
+    private List<Button> categoryButtons = new();
+
+    private void Start()
+    {
+        GenerateCategoryButtons();
+        UpdateCategoryVisuals();
+    }
 
     private void Update()
     {
@@ -29,5 +41,44 @@ public class LevelEditorManager : MonoBehaviour
     public GameObject GetSelectedTile()
     {
         return tileCategories[selectedCategoryIndex].variants[selectedVariantIndex];
+    }
+
+    private void GenerateCategoryButtons()
+    {
+        foreach (var category in tileCategories)
+        {
+            GameObject buttonObj = Instantiate(categoryButtonPrefab, categoryBar);
+            Button button = buttonObj.GetComponent<Button>();
+
+            Image img = buttonObj.GetComponentInChildren<Image>();
+            if (category.variants.Length > 0)
+            {
+                Sprite categorySprite = category.variants[0].GetComponent<SpriteRenderer>().sprite;
+                img.sprite = categorySprite;
+            }
+
+            int index = categoryButtons.Count;
+            button.onClick.AddListener(() => SelectCategory(index));
+            categoryButtons.Add(button);
+        }
+    }
+
+    private void SelectCategory(int index)
+    {
+        selectedCategoryIndex = index;
+        selectedVariantIndex = 0;
+        UpdateCategoryVisuals();
+    }
+
+    private void UpdateCategoryVisuals()
+    {
+        for (int i = 0; i < categoryButtons.Count; i++)
+        {
+            Image img = categoryButtons[i].GetComponent<Image>();
+            if (i == selectedCategoryIndex)
+                img.color = Color.yellow;
+            else
+                img.color = Color.white;
+        }
     }
 }
